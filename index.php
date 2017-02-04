7c34cd1c246b2992fcb563d84c0fbe11077bdf1b
root <root@ubuntu.ubuntu-domain>
2017-02-04 16:35:04 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include_once('header.php');
+?>
+ 
+
+            <!--Home Sections-->
+
+<div class="container">
+
+</div>
+  <div class="top-image">
+  	<img src="">	
+  </div>
+<?php
+include_once('footer.php');
+?>
